d95bd2a4f296a7afe24c79dfad0895cf9612e1b3
Erik Vullings <erik.vullings@gmail.com>
2018-02-19 18:25:58 +0100

    Added color codes to the console-logger from https://stackoverflow.com/a/41407246/319711

--- src/lib/logger/console-logger.ts
+++ src/lib/logger/console-logger.ts
@@ -1,17 +1,22 @@
 import { ILog } from './logger';
 import { LogLevel } from './log-levels';
 
+/**
+ * Console logger.
+ *
+ * Color codes are taken from [here](https://stackoverflow.com/a/41407246/319711).
+ */
 export class ConsoleLogger implements ILog {
   log(level: LogLevel, msg: string, _callback?: (err: any, result: any) => void) {
     switch (level) {
       case LogLevel.Error:
-        console.error(msg);
+        console.error('\x1b[1;31m%s\x1b[0m', msg);
         break;
       case LogLevel.Warn:
-        console.warn(msg);
+        console.warn('\x1b[33m%s\x1b[0m', msg);
         break;
       case LogLevel.Info:
-        console.info(msg);
+        console.info('\x1b[1;34m%s\x1b[0m', msg);
         break;
       default:
         console.log(msg);
