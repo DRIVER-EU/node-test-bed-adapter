2eeeb942fbebdeebaa303e72ef87b5ad8e503092
Erik Vullings <erik.vullings@gmail.com>
2018-03-02 11:03:28 +0100

    Export IDefaultKey interface.

--- src/lib/avro/default-key-schema.ts
+++ src/lib/avro/default-key-schema.ts
@@ -1,3 +1,49 @@
+/**
+ * The default key, as used in the test-bed, is based on the required fields of an EDXL 2.0 Distribution Element.
+ */
+export interface IDefaultKey {
+  /**
+   * The unique identifier of this distribution message.
+   */
+  distributionID: string;
+  /**
+   * The unique identifier of the sender.
+   */
+  senderID: string;
+  /**
+   * The date and time the distribution message was sent as the number of milliseconds
+   * from the UNIX epoch, 1 January 1970 00:00:00.000 UTC.
+   */
+  dateTimeSent: number;
+  /**
+   * The date and time the distribution message should expire as the number of milliseconds
+   * from the UNIX epoch, 1 January 1970 00:00:00.000 UTC.
+   */
+  dateTimeExpires: number;
+  /**
+   * The action-ability of the message.
+   */
+  distributionStatus: 'Actual' | 'Exercise' | 'System' | 'Test' | 'Unknown' | 'NoAppropriateDefault';
+  /**
+   * The function of the message.
+   */
+  distributionKind:
+    | 'Report'
+    | 'Update'
+    | 'Cancel'
+    | 'Request'
+    | 'Response'
+    | 'Dispatch'
+    | 'Ack'
+    | 'Error'
+    | 'SensorConfiguration'
+    | 'SensorControl'
+    | 'SensorStatus'
+    | 'SensorDetection'
+    | 'Unknown'
+    | 'NoAppropriateDefault';
+}
+
 export const defaultKeySchema = {
   type: 'record',
   name: 'EDXLDistribution',
