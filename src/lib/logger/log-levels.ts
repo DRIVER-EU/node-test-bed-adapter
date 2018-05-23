155fbe322f253f2e80b816f491b0f93761c00007
Erik Vullings <erik.vullings@gmail.com>
2018-05-23 19:25:13 +0200

    Updated log levels: info is less detailed than debug.

--- src/lib/logger/log-levels.ts
+++ src/lib/logger/log-levels.ts
@@ -5,12 +5,12 @@ export type LogLevelType = 'SILLY' | 'INFO' | 'DEBUG' | 'WARN' | 'ERROR' | 'CRIT
  */
 export enum LogLevel {
   Sill = 0,
-  Info = 1,
-  Debug = 2,
+  Debug = 1,
+  Info = 2,
   Warn = 3,
   Error = 4,
   Critical = 5
 }
 
-const LogLevels = [ 'SILLY', 'INFO', 'DEBUG', 'WARN', 'ERROR', 'CRITICAL' ] as LogLevelType[];
+const LogLevels = [ 'SILLY', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL' ] as LogLevelType[];
 export const LogLevelToType = (logLevel?: LogLevel): LogLevelType | undefined => logLevel ? LogLevels[logLevel] : undefined;
